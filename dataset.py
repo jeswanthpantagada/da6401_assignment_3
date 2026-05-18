010634fb4fb792908f58963bdbccf3dbbe1d8362
jeswanthpantagada <ee23b052@smail.iitm.ac.in>
2026-05-18 12:42:24 +0530

    Load Multi30k dataset and process data
    Implemented dataset loading, vocabulary building, and data processing for Multi30k dataset.

--- dataset.py
+++ dataset.py
@@ -1,26 +1,56 @@
-class Multi30kDataset:
+import torch
+from torch.utils.data import Dataset
+from datasets import load_dataset
+import spacy
+from torch.nn.utils.rnn import pad_sequence
+
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
+
+class Multi30kDataset(Dataset):
     def __init__(self, split='train'):
-        """
-        Loads the Multi30k dataset and prepares tokenizers.
-        """
         self.split = split
-        # Load dataset from Hugging Face
-        # https://huggingface.co/datasets/bentrevett/multi30k
-        # TODO: Load dataset, load spacy tokenizers for de and en
-        pass
+        self.dataset = load_dataset("bentrevett/multi30k", split=self.split)
+        self.vocab_de = {'<pad>': 0, '<sos>': 1, '<eos>': 2, '<unk>': 3}
+        self.vocab_en = {'<pad>': 0, '<sos>': 1, '<eos>': 2, '<unk>': 3}
+        self.data = []
+        
+        self.build_vocab()
+        self.process_data()
 
     def build_vocab(self):
-        """
-        Builds the vocabulary mapping for src (de) and tgt (en), including:
-        <unk>, <pad>, <sos>, <eos>
-        """
-        # TODO: Create the vocabulary dictionaries or torchtext Vocab equivalent
-        raise NotImplementedError
+        train_data = load_dataset("bentrevett/multi30k", split='train')
+        for item in train_data:
+            for tok in spacy_de.tokenizer(item['de']):
+                if tok.text not in self.vocab_de:
+                    self.vocab_de[tok.text] = len(self.vocab_de)
+            for tok in spacy_en.tokenizer(item['en']):
+                if tok.text not in self.vocab_en:
+                    self.vocab_en[tok.text] = len(self.vocab_en)
 
     def process_data(self):
-        """
-        Convert English and German sentences into integer token lists using
-        spacy and the defined vocabulary. 
-        """
-        # TODO: Tokenize and convert words to indices
-        raise NotImplementedError
+        for item in self.dataset:
+            de_tokens = [self.vocab_de.get(tok.text, 3) for tok in spacy_de.tokenizer(item['de'])]
+            en_tokens = [self.vocab_en.get(tok.text, 3) for tok in spacy_en.tokenizer(item['en'])]
+            
+            de_tensor = torch.tensor([1] + de_tokens + [2])
+            en_tensor = torch.tensor([1] + en_tokens + [2])
+            
+            self.data.append((de_tensor, en_tensor))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+def collate_fn(batch):
+    de_batch, en_batch = [], []
+    for de_item, en_item in batch:
+        de_batch.append(de_item)
+        en_batch.append(en_item)
+    
+    de_batch = pad_sequence(de_batch, padding_value=0, batch_first=True)
+    en_batch = pad_sequence(en_batch, padding_value=0, batch_first=True)
+    
+    return de_batch, en_batch
